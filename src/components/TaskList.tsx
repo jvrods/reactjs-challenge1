285c35b4bc59c3095b31220b3d715df610e540cf
João Victor Rodrigues <joaorodriguesvictor@outlook.com>
2022-02-27 22:24:28 -0300

    Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,38 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    const newTask = {                           // Aqui a nova task com o que ela recebe que ta definido na interface
+      id: Math.random(),                         //id aleatória
+      title: newTaskTitle,                        // o título que vou passar
+      isComplete: false                            //o isComplete  é um boolean 
+    }
+
+    setTasks(oldState => [...oldState, newTask]);   //setando a nova task (usando o spreed operation ("...") para pegar o que ja estava no array pegando o que ja tinha usando o "oldstate" e passando o newTask.
+    setNewTaskTitle('');                            // assim que a task é adicionando o campo de setar fica vazio novamente.
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {  //usamos a função map pra ir em cada uma das tasks passando as ids conferindo se é igual para adicionar o checked de completa ou não.
+      ...task,
+      isComplete : !task.isComplete
+    }: task);
+
+    setTasks(newTasks)
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTasks);
   }
 
   return (
